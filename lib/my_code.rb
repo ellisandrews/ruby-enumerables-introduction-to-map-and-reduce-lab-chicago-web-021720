a32c16df0f6ae93bb7fe5dfb80c843be86451e50
Ellis Andrews <ellisandrews1@gmail.com>
2020-01-23 04:14:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def map_to_no_change(array)
 end
 
 def map_to_double(array)
-    new_array = []
+  new_array = []
   array.length.times do |i|
     new_array << array[i] * 2
   end
@@ -23,7 +23,7 @@ def map_to_double(array)
 end
 
 def map_to_double(array)
-    new_array = []
+  new_array = []
   array.length.times do |i|
     new_array << array[i] ** 2
   end
